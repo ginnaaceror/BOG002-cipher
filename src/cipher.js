1617a04243ac05e036d0196a78d101bcc4d3e183
Hohjanna <hohjanna@MacBook-Pro-de-Hohjanna.local>
2021-03-02 11:29:26 -0500

    subiendo proyecto cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,52 @@
 const cipher = {
-  // ...
-};
+    cifrado,
+    descifrado
+}
+
+function cifrado(texto, posicion) {
+    let resultado = "";
+    let cadena = "ABCDEFGHIJKLMNOPQRTSUVWXYZ";
+
+
+    posicion = (posicion % 26 + 26) % 26;
+
+    if (texto) {
+        for (let i = 0; i < texto.length; i++) {
+            if (cadena.indexOf(texto[i]) != -1) {
+                let cadenaPosicion = ((cadena.indexOf(texto[i]) + posicion) % 26);
+                resultado += cadena[cadenaPosicion];
+            } else {
+                resultado += texto[i];
+            }
+        };
+
+    }
+    return resultado;
+
+}
+
+function descifrado(texto, posicion) {
+    let resultado = "";
+    let cadena = "ABCDEFGHIJKLMNOPQRTSUVWXYZ";
+
+    posicion = (posicion % 26 - 26) % 26;
+
+    if (texto) {
+        for (let i = 0; i < texto.length; i++) {
+            let indiceEncontrado = cadena.indexOf(texto[i])
+            if (indiceEncontrado != -1) {
+                let cadenaPosicion = ((indiceEncontrado - posicion) % 26);
+                resultado += cadena[cadenaPosicion];
+            } else
+                resultado += texto[i];
+        };
+
+
+
+    }
+    return resultado;
+
+}
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 import cipher from './cipher.js';
+//cipher.descifrado("B", 1)
 
-console.log(cipher);
+
+/*id de la sección cifrado*/
+
+let texto = document.getElementById("denuncia");
+let posicion = document.getElementById("desplazamiento");
+let btncifrado = document.getElementById("btncifrado1")
+let resultado = document.getElementById("textoCifrado");
+
+
+
+texto.addEventListener("keyup", function() {
+    texto.value = texto.value.toUpperCase();
+
+});
+
+
+
+/*Un evento es inutil sino se le asigna alguna función, en este caso el 
+evento "click" invoca la función de cifrar por medio de btncifrado1()*/
+
+btncifrado.addEventListener("click", btncifrado1)
+
+
+function btncifrado1() {
+    let textoIngresado = texto.value;
+    let numeroIngresado = posicion.value;
+    let resultadoCifrado = resultado.value = cipher.cifrado(textoIngresado, numeroIngresado);
+}
+
+
+/*id de la sección descifrado*/
+
+let textoCifrado = document.getElementById("denunciaCifrada");
+let btndescifrado = document.getElementById("btndescifrado1")
+let salida = document.getElementById("textoDescifrado");
+
+textoCifrado.addEventListener("keyup", function() {
+    textoCifrado.value = textoCifrado.value.toUpperCase();
+
+});
+
+
+
+btndescifrado.addEventListener("click", btndescifrado1)
+
+function btndescifrado1() {
+    let textoIngresado2 = textoCifrado.value;
+    let posicionActual = posicion.value;
+    let mensajeFinal = salida.value = cipher.descifrado(textoIngresado2, posicionActual);
+}
+
+/*Necesitamos 2 "parametros" para que la función descifrado 
+tome los 2 valores de: mensaje cifrado y además la posición en la 
+cual se encuentra cada letra (por eso salía undefined) ya que como parametro solo estaba
+textoIngresado2*/
+
+/*function btndescifrado1() {
+    let textoIngresado2 = textoCifrado.value;
+    let resultadoDescifrado = resultado2.value = cipher.descifrado(textoIngresado2);
+}*/
